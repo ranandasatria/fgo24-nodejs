ab062e73318969fce70704c39240f8cd896283c8
ranandasatria <rananda24@gmail.com>
2025-04-22 14:27:08 +0700

    Add files via upload

---
+++ minitask1.js
@@ -0,0 +1,31 @@
+const fs = require('fs').promises
+const path = require('path')
+
+async function createFile(fileName) {
+    try{
+        const content = "Hello\n"
+        await fs.writeFile(fileName, content)
+        console.log("write file success")
+    }catch(err){
+        console.log(err)
+    }
+}
+
+const listMusic = [
+    'The Strokes - Reptilia', 
+    'The Strokes - Selfess', 
+    'The Strokes - Soma',
+    'Kangen Band - Yolanda',
+    'Sound Horeg',
+    'Wali - Yank',
+    'SNSD - Gee',
+    'JKT48 - Shonichi',
+    'Silampukau - Balada Harian',
+    'Iwan Fals - Cikal'    
+]
+
+listMusic.forEach(music=>{
+    createFile(path.join("music", `${music}.mp3`))
+})
+
+// createFile(path.join("music", "example.txt"))
